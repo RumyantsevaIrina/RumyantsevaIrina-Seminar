1dfa3d198535974afb86611bdbe21bf12bcf8c29
Irina Rumyantseva <rumyantseva-i@yandex.ru>
2024-02-02 16:17:22 +0300

    Добавление seminar2-DZ4

---
+++ seminar2-DZ4/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, которая на вход принимает натуральное число N,
+// а на выходе показывает его цифры через запятую.
+
+namespace NumberDigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите натуральное число N:");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Цифры числа {0}:", number);
+
+            string digits = GetNumberDigits(number);
+            Console.WriteLine(digits);
+        }
+        static string GetNumberDigits(int number)
+        {
+            string digitsString = "";
+
+            while (number > 0)
+            {
+                int digit = number % 10;
+                digitsString = digit + "," + digitsString;
+                number = number / 10;
+            }
+
+            // Удаляем последнюю запятую
+            digitsString = digitsString.TrimEnd(',');
+
+            return digitsString;
+        }
+    }
+}
