203658a6c9735063ccc697afa79256b7a8145f13
Irina Rumyantseva <rumyantseva-i@yandex.ru>
2024-02-02 14:49:30 +0300

    Добавление seminar2-DZ3

---
+++ seminar2-DZ3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает 
+// на вход целое число из отрезка [10, 99] 
+// и показывает наибольшую цифру числа.
+
+Console.WriteLine("Введите целое число от 10 до 99:");
+            int number = int.Parse(Console.ReadLine());
+
+            int maxDigit = 0;
+            int tempDigit;
+
+            while (number > 0)
+            {
+                tempDigit = number % 10;
+                if (tempDigit > maxDigit)
+                {
+                    maxDigit = tempDigit;
+                }
+                number = number / 10;
+            }
+
+            Console.WriteLine("Наибольшая цифра в числе: " + maxDigit);
