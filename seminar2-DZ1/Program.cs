25665a9a8d151477fb6b244d16fecfd8f6657dd2
Irina Rumyantseva <rumyantseva-i@yandex.ru>
2024-02-02 12:34:32 +0300

    Добавление seminar2-DZ1

---
+++ seminar2-DZ1/Program.cs
@@ -0,0 +1,19 @@
+﻿﻿// Напишите программу, которая принимает на вход число и проверяет, 
+// кратно ли оно одновременно 7 и 23
+
+int GetUserInput(string str)
+{
+    Console.Write($"{str}: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+bool DivisionTo7and23(int num)
+{
+    return num % 7 == 0 && num % 23 == 0;
+}
+
+int number = GetUserInput("Enter a number: ");
+bool result = DivisionTo7and23(number);
+
+Console.Write(result ? $"{number} - да" : $"{number} - нет");
