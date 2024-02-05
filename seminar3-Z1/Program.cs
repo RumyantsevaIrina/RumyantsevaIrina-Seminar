2d19bc5dd7a4a5c97d7b9791746ff343ae5d0749
Irina Rumyantseva <rumyantseva-i@yandex.ru>
2024-02-05 13:25:19 +0300

    Добавление seminar3-Z1

---
+++ seminar3-Z1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задание 1. Совместная работа
+// Задайте массив. Напишите программу, которая определяет,
+// присутствует ли заданное число в массиве. Программа
+// должна выдать ответ: Да/Нет.
+// Примеры
+// [1 3 4 19 3], 8 => Нет
+// [-4 3 4 1], 3 => Да
+
+int[] array = { 1, 3, 4, 19, 3 };
+int number = 19;
+bool result = false;
+for (int i = 0; i < array.Length; i++)
+{
+if (array[i] == number)
+{
+result = true;
+break;
+}
+}
+if (result == true)
+{
+System.Console.WriteLine("YES");
+}
+else
+{
+System.Console.WriteLine("No");
+}
