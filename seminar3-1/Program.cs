0bdcb07a3d2bcfce03b632f001979e0645e97a40
Irina Rumyantseva <rumyantseva-i@yandex.ru>
2024-02-05 12:55:13 +0300

    Добавление seminar3-1

---
+++ seminar3-1/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите размер массива: ");
+int size = int.Parse(Console.ReadLine()!);
+
+//int[] numbers = { 5, 123, 12312312 };
+int[] numbers = new int[size];
+
+Console.WriteLine($"Введите {size} элементов массива:");
+for (int i = 0; i < size; i++)
+{
+int input = int.Parse(Console.ReadLine()!);
+numbers[i] = input;
+}
+
+
+Console.WriteLine("Массив:");
+for (int i = 0; i < numbers.Length; i++)
+{
+Console.Write(numbers[i] + " ");
+}
