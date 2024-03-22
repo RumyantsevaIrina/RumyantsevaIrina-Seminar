eeb2d4a4981003931f40b6337ba3652152421c19
Irina Rumyantseva <rumyantseva-i@yandex.ru>
2024-03-22 16:03:15 +0300

    Добавление  папки seminar4-DZ2

---
+++ seminar4-DZ2/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 2: Задайте массив заполненный 
+// случайными трёхзначными числами. 
+// Напишите программу, которая покажет 
+// количество чётных чисел в массиве.
+class Program
+{
+    static void Main()
+    {
+        int[] array = new int[10]; // Создаем массив из 10 элементов
+        Random random = new Random();
+
+        // Заполняем массив случайными трехзначными числами
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = random.Next(100, 1000);
+        }
+
+        int countEven = 0;
+
+        // Подсчитываем количество четных чисел в массиве
+        foreach (int num in array)
+        {
+            if (num % 2 == 0)
+            {
+                countEven++;
+            }
+        }
+
+        Console.WriteLine("Массив случайных трехзначных чисел:");
+        foreach (int num in array)
+        {
+            Console.Write(num + " ");
+        }
+
+        Console.WriteLine("\nКоличество четных чисел в массиве: " + countEven);
+    }
+}
