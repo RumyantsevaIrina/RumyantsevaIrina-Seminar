6bc31aac741f5faff49437dd7314929d2994c258
Irina Rumyantseva <rumyantseva-i@yandex.ru>
2024-03-23 14:09:48 +0300

    Добавление  папки seminar4-DZ3

---
+++ seminar4-DZ3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая перевернёт одномерный массив 
+// (первый элемент станет последним, второй – предпоследним и т.д.)
+class Program
+{
+    static void Main()
+    {
+        int[] array = { 1, 2, 3, 4, 5 };
+        
+        Array.Reverse(array);
+        
+        foreach (var item in array)
+        {
+            Console.Write(item + " ");
+        }
+    }
+}
