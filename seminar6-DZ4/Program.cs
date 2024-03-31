9e943f1544f51a014391ebb9f05e9197fefd15ad
Irina Rumyantseva <rumyantseva-i@yandex.ru>
2024-03-31 16:55:06 +0300

    Добавление  папки seminar6-DZ4

---
+++ seminar6-DZ4/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задайте строку, состоящую из слов, разделенных пробелами. 
+// Сформировать строку, в которой слова расположены в обратном порядке. 
+// В полученной строке слова должны быть также разделены пробелами.
+string inputString = "Hello World It's a Beautiful Day"; // исходная строка
+
+string[] words = inputString.Split(' '); // разбиваем строку на слова
+
+Array.Reverse(words); // переворачиваем массив слов
+
+string reversedString = String.Join(" ", words); // объединяем слова обратно в строку с пробелами
+
+Console.WriteLine(reversedString);
