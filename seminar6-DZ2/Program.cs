5e3b2a0d5d9499869f7376ebd3797a7cf8b65b3d
Irina Rumyantseva <rumyantseva-i@yandex.ru>
2024-03-31 16:44:07 +0300

    Добавление  папки seminar6-DZ2

---
+++ seminar6-DZ2/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задайте строку, содержащую латинские буквы в обоих регистрах. 
+// Сформируйте строку, в которой все заглавные буквы заменены на строчные.
+
+
+string inputString = "Hello World";
+string result = inputString.ToLower();
+
+Console.WriteLine(result);
+
