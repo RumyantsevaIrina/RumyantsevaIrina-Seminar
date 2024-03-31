158e7866351e271991868a0abb79a3b142e488ff
Irina Rumyantseva <rumyantseva-i@yandex.ru>
2024-03-31 16:39:10 +0300

    Добавление  папки seminar6-DZ1

---
+++ seminar6-DZ1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задайте двумерный массив символов (тип char [,]). 
+// Создать строку из символов этого массива.
+char[,] charArray = { { 'H', 'e', 'l', 'l', 'o' }, { 'W', 'o', 'r', 'l', 'd' } };
+string result = String.Empty;
+
+for (int i = 0; i < charArray.GetLength(0); i++)
+{
+    for (int j = 0; j < charArray.GetLength(1); j++)
+    {
+        result += charArray[i, j];
+    }
+}
+
+Console.WriteLine(result);
