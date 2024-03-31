52968cd29fad781072157cba0ba02397ed849df4
Irina Rumyantseva <rumyantseva-i@yandex.ru>
2024-03-31 16:49:37 +0300

    Добавление  папки seminar6-DZ3

---
+++ seminar6-DZ3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задайте произвольную строку. 
+// Выясните, является ли она палиндромом.
+
+string inputString = "А роза упала на лапу Азора"; // произвольная строка
+
+// Удаляем из строки все пробелы и приводим к нижнему регистру
+string cleanString = String.Join("", inputString.ToLower().Where(Char.IsLetter));
+
+// Создаем строку-палиндром путем обращения исходной строки
+string reversedString = new string(cleanString.Reverse().ToArray());
+
+bool isPalindrome = cleanString == reversedString;
+
+if (isPalindrome)
+{
+    Console.WriteLine("Данная строка является палиндромом.");
+}
+else
+{
+    Console.WriteLine("Данная строка не является палиндромом.");
+}
