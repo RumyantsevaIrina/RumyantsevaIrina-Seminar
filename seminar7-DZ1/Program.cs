9ccc3e2428c2bd580c2031a80caa81afea9effbf
Irina Rumyantseva <rumyantseva-i@yandex.ru>
2024-04-04 12:09:29 +0300

    Добавление  папки seminar7-DZ!

---
+++ seminar7-DZ1/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задайте значения M и N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от M до N.
+
+int n = Convert.ToInt32(Console.ReadLine());
+int m = Convert.ToInt32(Console.ReadLine());
+void F(int m, int n)
+{
+    if (m != n)
+    {
+        if (m > n)
+        {
+            F(m - 1, n);
+            System.Console.Write(m + " ");
+        }
+        else
+        {
+
+            F(m + 1, n);
+            System.Console.Write(m + " ");
+
+        }
+
+    }
+    else
+    {
+        System.Console.Write(m + " ");
+    }
+}
+
+F(m, n);
