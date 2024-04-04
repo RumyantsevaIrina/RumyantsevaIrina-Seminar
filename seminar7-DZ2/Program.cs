ec1fd607bdbf24596e6f9424b0788bced0924866
Irina Rumyantseva <rumyantseva-i@yandex.ru>
2024-04-04 13:03:04 +0300

    Добавление  папки seminar7-DZ2

---
+++ seminar7-DZ2/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+using System;
+
+class AckermannFunction
+{
+    static int Ackermann(int m, int n)
+    {
+        if (m == 0)
+        {
+            return n + 1;
+        }
+        else if (m > 0 && n == 0)
+        {
+            return Ackermann(m - 1, 1);
+        }
+        else
+        {
+            return Ackermann(m - 1, Ackermann(m, n - 1));
+        }
+    }
+
+    static void Main()
+    {
+        int m = 3; // значение m
+        int n = 2; // значение n
+        int result = Ackermann(m, n);
+        Console.WriteLine($"Значение функции Аккермана для m={m} и n={n} равно {result}.");
+    }
+}
